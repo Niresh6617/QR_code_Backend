ab5bec568e1145968e8d6cd169206089c7486d6b
Niresh6617 <niresh6617@gmail.com>
2025-01-24 23:48:40 +0530

    update commands

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ const qrcode= require('qrcode')
 
 //Express application setup
 const app = express();
+// why we use ejs in html because it allows us to embed dynamic content into HTML templates.
+// or it converts the html code dynamically EJS(Embeded Javascript )
 
 //View Engine Configuration
 //view engine: Sets EJS as the template engine to render dynamic HTML.
@@ -16,6 +18,7 @@ app.set("views",path.join(__dirname,"views"))
 
 //middleware setup
 //express.json(): Parses JSON payloads from incoming requests.
+// parse means analyze and converting data form one formate to another which is understood by program
 // express.urlencoded({ extended: true })means it convert the data into url which is submitted through form tag
 // express.static('public') means it serves static files from public directory
 
@@ -27,10 +30,10 @@ app.get('/',(request,response)=>{
     response.render('index') 
     
 })
-// why we use response.render(filename) instead of using response.sendFile(filename) because
-// the file index.ejs contains dynamic placeholdersso we use render
+// why we use response.render(filename) instead of using response.sendFile(path.join(__dirname,'filename')) because
+// the file index.ejs contains dynamic placeholders so we use render
 //whenever the file extension has .html it means that file is a static file so we use sendFile(filename)
-
+// after that go to views folder inside index.ejs file 
 
 // app.post means we will get information from browser using form
 app.post('/qrcodedata',(req,res)=>{
